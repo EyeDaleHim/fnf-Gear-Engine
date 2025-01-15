4d0882bece72a473dd59acbb2c859a5be0db13ed
EyeDaleHim <71162374+EyeDaleHim@users.noreply.github.com>
2025-01-15 11:43:11 +0800

    healthbar and icon change

--- source/objects/play/Healthbar.hx
+++ source/objects/play/Healthbar.hx
@@ -7,10 +7,14 @@ import flixel.group.FlxContainer;
 
 class Healthbar extends FlxContainer
 {
+    public var value(get, set):Float;
+
     public var bar:FlxBar;
 
     public var leftIcon:Icon;
     public var rightIcon:Icon;
+
+    public var iconBarSpeed:Float = 2.5;
 
     private var _barValue:Float = 0.0;
 
@@ -28,12 +32,77 @@ class Healthbar extends FlxContainer
 
         leftIcon = new Icon(Icon.gameplayPath, "dad");
         leftIcon.changeScale = true;
+
+        leftIcon.animation.add('normal', [0], 1.0);
+        leftIcon.animation.add('losing', [1], 1.0);
+
         add(leftIcon);
 
         rightIcon = new Icon(Icon.gameplayPath, "bf");
         rightIcon.changeScale = true;
+        rightIcon.flipX = true;
         add(rightIcon);
 
-        _barValue = bar.value;
+        rightIcon.animation.add('normal', [0], 1.0);
+        rightIcon.animation.add('losing', [1], 1.0);
+
+        leftIcon.animation.play('normal');
+        rightIcon.animation.play('normal');
+
+        _barValue = value;
+        set_value(value);
+    }
+
+    override public function update(elapsed:Float)
+    {
+        super.update(elapsed);
+
+        _barValue = FlxMath.lerp(_barValue, value, FlxMath.bound(elapsed * 4.0 * iconBarSpeed, 0, 1));
+
+        var range:Float = bar.max - bar.min;
+        var percent:Float = ((_barValue - bar.min) / range) * 100.0;
+
+        leftIcon.x = FlxMath.remapToRange(percent, bar.min, bar.max, bar.x + bar.width, bar.x);
+        leftIcon.x -= leftIcon.width - 10.0;
+
+        rightIcon.x = FlxMath.remapToRange(percent, bar.min, bar.max, bar.x + bar.width, bar.x);
+        rightIcon.x -= 10.0;
+
+        leftIcon.y = bar.y + bar.height - (leftIcon.height / 1.6);
+        rightIcon.y = bar.y + bar.height - (rightIcon.height / 1.6);
+    }
+
+    public function beatHit(beat:Int)
+    {
+        if (beat >= 0)
+        {
+            leftIcon.scale.set(1.2, 1.2);
+            rightIcon.scale.set(1.2, 1.2);
+
+            leftIcon.updateHitbox();
+            rightIcon.updateHitbox();
+        }
+    }
+
+    function get_value():Float
+    {
+        return bar.value;
+    }
+
+    function set_value(value:Float):Float
+    {
+        bar.value = value;
+
+        if (bar.percent > 80)
+            leftIcon.animation.play('losing');
+        else
+            leftIcon.animation.play('normal');
+
+        if (bar.percent < 20)
+            rightIcon.animation.play('losing');
+        else
+            rightIcon.animation.play('normal');
+
+        return value;
     }
 }
